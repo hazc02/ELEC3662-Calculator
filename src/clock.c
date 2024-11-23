0d5ef08fe03c184e95828e408569b57251a119e0
Harry <hacarless@gmail.com>
2024-11-23 10:28:57 +0000

    [-] - Defines removed from clock.c after creating header file

--- src/clock.c
+++ src/clock.c
@@ -1,12 +1,4 @@
-// PPL Defines //
-#define SYSCTL_RIS_R 		  (*((volatile unsigned long *)0x400FE050)) 
-#define SYSCTL_RCC_R 		  (*((volatile unsigned long *)0x400FE060))
-#define SYSCTL_RCC2_R         (*((volatile unsigned long *)0x400FE070))	
-
-// SysTick Defines	//
-#define NVIC_ST_CTRL_R        (*((volatile unsigned long *)0xE000E010))
-#define NVIC_ST_RELOAD_R      (*((volatile unsigned long *)0xE000E014))
-#define NVIC_ST_CURRENT_R     (*((volatile unsigned long *)0xE000E018))
+#include "clock.h";
 
 void SysTick_init(void){
 	// Disable SysTick during Setup, ensuring the timer is off whilst configuring
