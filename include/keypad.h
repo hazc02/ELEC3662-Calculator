97906ee38c34da3d44bd700127c928c460aa009b
hazc02 <hacarless@gmail.com>
2024-11-30 23:25:05 +0000

    [+] - Keypad.h created

---
+++ include/keypad.h
@@ -0,0 +1,10 @@
+#ifndef KEYPAD_H
+#define KEYPAD_H
+
+/**
+ * @brief Scans the keypad and returns the character corresponding to the pressed key.
+ * @return The character of the pressed key, or '\0' if no key is pressed.
+ */
+char Keypad_GetKey(void);
+
+#endif 
